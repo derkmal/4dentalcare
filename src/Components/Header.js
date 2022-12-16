1ebba9e87deefea8c05fbf85390ea61225737fbf
darkmal <vagnerwillians@hotmail.com>
2022-12-16 17:37:11 -0300

    16-12

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header=()=> {
+    return(
+        <div className="header">
+            <h2>Display</h2>
+            <h2>Blog</h2>
+        </div>
+    );
+};
+
+export default Header;
