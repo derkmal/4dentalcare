4503719b113b8aaebc6f37f4ff7116ee4a6c21f0
darkmal <vagnerwillians@hotmail.com>
2022-12-18 09:46:19 -0300

    18-12

--- src/Components/Meeting.js
+++ src/Components/Meeting.js
@@ -6,7 +6,7 @@ const Meeting=()=> {
             <h1>Receive the best dental treatment to avoid crooked-teeth problem</h1>
             <h2>You will receve the proper assessment to find out which braces best suit you.</h2>
             <button>Bring back your smile now</button>
-            <h3>Well aligned teeth and fresher breath always leave a good impression when meeting a person for the first time.</h3>
+            <h3>Well-aligned teeth and fresher breath always leave a good impression when meeting a person for the first time.</h3>
         </div>
     );
 };
