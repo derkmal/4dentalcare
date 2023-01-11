d84504b4de72bcd353aa59ab2dfb78b2cbe85686
darkmal <vagnerwillians@hotmail.com>
2023-01-11 20:03:55 -0300

    11-01-2023

---
+++ build/precache-manifest.f5502783dec0d4bc12bc2e658395967f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf0a7b1d8889cc313dd7",
+    "url": "/dentalcare/static/css/main.1fbaebac.chunk.css"
+  },
+  {
+    "revision": "bf0a7b1d8889cc313dd7",
+    "url": "/dentalcare/static/js/main.bf0a7b1d.chunk.js"
+  },
+  {
+    "revision": "7fe6c5c56602ae9f819b",
+    "url": "/dentalcare/static/js/1.7fe6c5c5.chunk.js"
+  },
+  {
+    "revision": "b70160d5bbb60568bb0f",
+    "url": "/dentalcare/static/js/runtime~main.b70160d5.js"
+  },
+  {
+    "revision": "781e2ae1df13de4f383908b7f113f39e",
+    "url": "/dentalcare/index.html"
+  }
+];
--- src/Components/Meeting.js
+++ src/Components/Meeting.js
@@ -4,7 +4,7 @@ const Meeting=()=> {
     return(
         <div>
             <h1>Receive the best dental treatment to avoid crooked-teeth problem</h1>
-            <h2>You will receve the proper assessment to find out which braces best suit you.</h2>
+            <h2>We will give you the accurate assessment to find out which braces best suit you.</h2>
             <button>Bring back your smile now</button>
             <h3>Well-aligned teeth and fresher breath always leave a good impression when meeting a person for the first time.</h3>
         </div>
