fbe7be481dfa01efd60b7e339de9fe579a51deb2
darkmal <vagnerwillians@hotmail.com>
2023-01-12 19:32:00 -0300

    Updates

---
+++ precache-manifest.41d3e80ac2a906b4a6f038d141b3bffd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5f47a34503f57edf38c",
+    "url": "/dentalcare/static/css/main.4e8d0ca4.chunk.css"
+  },
+  {
+    "revision": "e5f47a34503f57edf38c",
+    "url": "/dentalcare/static/js/main.e5f47a34.chunk.js"
+  },
+  {
+    "revision": "7fe6c5c56602ae9f819b",
+    "url": "/dentalcare/static/js/1.7fe6c5c5.chunk.js"
+  },
+  {
+    "revision": "b70160d5bbb60568bb0f",
+    "url": "/dentalcare/static/js/runtime~main.b70160d5.js"
+  },
+  {
+    "revision": "c7fc79377aa6a4d2b37cdfc71c1db374",
+    "url": "/dentalcare/index.html"
+  }
+];
