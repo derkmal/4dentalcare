e731f4d0daf650e18b4db833f6341fd354350c8c
darkmal <vagnerwillians@hotmail.com>
2023-01-15 22:15:15 -0300

    15-01-2023 22:15hs

--- src/Components/Job.js
+++ src/Components/Job.js
@@ -5,8 +5,8 @@ const Job=()=> {
     return(
         <div>
             <div className="jobInterview">
-                <img src="Images/2.jpg" alt="jobInterview" style={{ width: '200px' }} />    
-                <h3>Even in a job interview your smile can lead to a favourable situation</h3>
+                <img src="Images/2.jpg" alt="jobInterview" style={{ width: '400px' }} />    
+                <p>Even in a job interview your smile can lead to a favourable situation</p>
             </div>
             
             <button className="lastButton">Bring back your smile now</button>
--- src/Components/Meeting.js
+++ src/Components/Meeting.js
@@ -8,12 +8,12 @@ const Meeting=()=> {
             <div className="h1h2Button">
                 <h1>RECEIVE THE BEST DENTAL TREATMENT TO AVOID CROOKED-TEETH PROBLEM</h1>
                 <h2>We will give you the accurate assessment to find out which braces best suit you.</h2>
-                <button>Bring back your smile now</button>
+                <button className="firstButton">Bring back your smile now</button>
             </div>
 
             <div className="girlSmile">
-                <img src="Images/1.jpg" alt="Girl" style={{ width: '200px' }} />
-                <h3>Well-aligned teeth and fresher breath always leave a good impression when meeting a person for the first time.</h3>
+                <img src="Images/1.jpg" alt="Girl" style={{ width: '400px' }} />
+                <p>Well-aligned teeth and fresher breath always leave a good impression when meeting a person for the first time.</p>
             </div>
         </div>
     );
