3ef979254a9da64f15f3b1c23058438163a84e2b
darkmal <vagnerwillians@hotmail.com>
2023-01-21 11:35:55 -0300

    21-01-2023

--- src/App.js
+++ src/App.js
@@ -4,27 +4,30 @@ import Meeting from './Components/Meeting';
 import './App.css';
 import Data from './Components/MeetingData';
 
-const App=()=> {
-    return(
-        <>
-            <Header />
-            <div className="containerHeader">
-                <h1>RECEIVE THE BEST DENTAL TREATMENT TO AVOID CROOKED-TEETH PROBLEM</h1>
-                <h2>We will give you the accurate assessment to find out which braces best suit you.</h2>
-            </div>
-            {Data.map((values)=>{
-                return(
-                    <>
-                        <Meeting 
-                            key={values.id}
-                            imgsrc={values.imgsrc}
-                            text={values.text}
-                        />
-                    </>
-                )
-            })}
-        </>
-    );
-};
+    const App=()=> {
+        return(
+            <>
+                <Header />
+                <div className="containerHeader">
+                    <h1>RECEIVE THE BEST DENTAL TREATMENT TO AVOID CROOKED-TEETH PROBLEM</h1>
+                    <h2>We will give you the accurate assessment to find out which braces best suit you.</h2>
+                </div>
+                
+                <div className="containerMeeting">
+                    {Data.map((values)=>{
+                        return(
+                            <>
+                                <Meeting 
+                                    key={values.id}
+                                    imgsrc={values.imgsrc}
+                                    text={values.text}
+                                />
+                            </>
+                        )
+                    })}
+                </div>
+            </>
+        );
+    };
 
 export default App;
