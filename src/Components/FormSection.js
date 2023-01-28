587d1f7ed8fab42dc5abbec7fcce6302e9631846
darkmal <vagnerwillians@hotmail.com>
2023-01-28 18:23:18 -0300

    28-01 18:23hs

--- src/Components/FormSection.js
+++ src/Components/FormSection.js
@@ -1,24 +1,42 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './FormSection.css'
 
 const FormSection = () => {
-  return (
+    const [myname, setmyName]=useState("");
+    const [mobile, setMobile]=useState("");
+    const [newEntry, setNewEntry]=useState([]);
+
+    const myData =(e)=> {
+        setmyName(e.target.value);
+    }
+    const myMobile =(e)=> {
+        setMobile(e.target.value);
+    }
+    const submitData =(e)=> {
+        e.preventDefault();
+        const newData={myname:myname, mobile:mobile};
+        setNewEntry([...newEntry, newData]);
+        setmyName("");
+        setMobile("");
+    }
+
+    return (
     <>
         <div className="containerFormSection">
             <div className="containerForm my-3 text-center">
-                <form>
+                <form onSubmit={submitData}>
                     <div>
-                        <input type="text" placeholder='Ihre Name schreiben' className='w-50 my-1 py-1' />
+                        <input type="text" placeholder='Ihre Name schreiben' className='w-50 my-1 py-1' value={myname} onChange={myData} />
                     </div>
                     <div>
-                        <input type="text" placeholder='Ihre Mobilnummer schreiben' className='w-50 my-1 py-1' />
+                        <input type="text" placeholder='Ihre Mobilnummer schreiben' className='w-50 my-1 py-1' value={mobile} onChange={myMobile} />
                     </div>
                     <button className="btn btn-dark" type='submit'>Submit</button>
                 </form>
             </div>
         </div>
     </>
-  )
+    )
 }
 
 export default FormSection;
