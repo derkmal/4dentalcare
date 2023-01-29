e65720e499d7f37acf6c972b555c47d388f7155a
darkmal <vagnerwillians@hotmail.com>
2023-01-29 20:21:43 -0300

    29-01-2023

--- src/Components/FormSection.js
+++ src/Components/FormSection.js
@@ -4,6 +4,8 @@ import './FormSection.css'
 const FormSection = () => {
     const [myname, setmyName]=useState("");
     const [mobile, setMobile]=useState("");
+    const [email, setEmail]=useState("");
+    const [password, setPassword]=useState("");
     const [newEntry, setNewEntry]=useState([]);
 
     const myData =(e)=> {
@@ -12,12 +14,20 @@ const FormSection = () => {
     const myMobile =(e)=> {
         setMobile(e.target.value);
     }
+    const myEmail =(e)=> {
+        setEmail(e.target.value);
+    }
+    const myPassword =(e)=> {
+        setPassword(e.target.value);
+    }
     const submitData =(e)=> {
         e.preventDefault();
-        const newData={myname:myname, mobile:mobile};
+        const newData={myname:myname, mobile:mobile, email:email, password:password};
         setNewEntry([...newEntry, newData]);
         setmyName("");
         setMobile("");
+        setEmail("");
+        setPassword("");
     }
 
     return (
@@ -26,10 +36,16 @@ const FormSection = () => {
             <div className="containerForm my-3 text-center">
                 <form onSubmit={submitData}>
                     <div>
-                        <input type="text" placeholder='Ihre Name schreiben' className='w-50 my-1 py-1' value={myname} onChange={myData} />
+                        <input type="text" placeholder='Ihren Name schreiben' className='w-50 my-1 py-1' value={myname} onChange={myData} />
                     </div>
                     <div>
                         <input type="text" placeholder='Ihre Mobilnummer schreiben' className='w-50 my-1 py-1' value={mobile} onChange={myMobile} />
+                    </div>
+                    <div>
+                        <input type="text" placeholder='Ihre E-mail schreiben' className='w-50 my-1 py-1' value={email} onChange={myEmail} />
+                    </div>
+                    <div>
+                        <input type="text" placeholder='Ihres Passwort schreiben' className='w-50 my-1 py-1' value={password} onChange={myPassword} />
                     </div>
                     <button className="btn btn-dark" type='submit'>Submit</button>
                 </form>
