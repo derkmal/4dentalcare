bff34c551572d38164e8b1ce95bd3ad6297cc743
darkmal <vagnerwillians@hotmail.com>
2023-02-23 22:28:43 -0300

    23-02-2023 22:28hs

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -9,25 +9,27 @@ const SignIn=()=> {
       </div>
       {/* https://thenounproject.com/ */}
 	  <form>
-			<div>
-				<img src="../images/name-vector.png" alt="name-photo" /> 
-				<input type="text" placeholder='Ihren Name schreiben'  value="" name="myname" />
-			</div>
-			<div>
-				<img src="../images/telephone-vector.png" alt="telephone-photo" />
-				<input type="text" placeholder='Ihre Mobilnummer schreiben' value=""  name="mobile" />
-			</div>
-			<div>
-				<img src="../images/email-vector2.png" alt="email-photo" />
-				<input type="text" placeholder='Ihre E-mail schreiben' value=""  name="email" />
-			</div>
-			<div>
-				<img src="../images/password-vector2.png" alt="password-photo" />
-				<input type="text" placeholder='Ihres Passwort schreiben'  value=""  name="password" />
-			</div>
-			
-			<div>
-				<input type='submit' className='forButton' value="I Want My Smile Back" />
+		   <div className="formContainer">
+				<div>
+					<img src="../images/name-vector.png" alt="name-photo" /> 
+					<input type="text" placeholder='Ihren Name schreiben'  value="" name="myname" />
+				</div>
+				<div>
+					<img src="../images/telephone-vector.png" alt="telephone-photo" />
+					<input type="text" placeholder='Ihre Mobilnummer schreiben' value=""  name="mobile" />
+				</div>
+				<div>
+					<img src="../images/email-vector2.png" alt="email-photo" />
+					<input type="text" placeholder='Ihre E-mail schreiben' value=""  name="email" />
+				</div>
+				<div>
+					<img src="../images/password-vector2.png" alt="password-photo" />
+					<input type="text" placeholder='Ihres Passwort schreiben'  value=""  name="password" />
+				</div>
+				
+				<div>
+					<input type='submit' className='forButton' value="I Want My Smile Back" />
+				</div>
 			</div>
 		</form>
 	  
