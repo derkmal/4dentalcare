c384ffce36a878c4ef872310a04fa6cf064d1a14
darkmal <vagnerwillians@hotmail.com>
2023-02-25 20:19:16 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dentalcare/precache-manifest.868ab9b382ac34c5b09d661bff2fd6bb.js"
+  "/dentalcare/precache-manifest.b525398a007cd9ea149a9f06f7f6275d.js"
 );
 
 workbox.clientsClaim();
