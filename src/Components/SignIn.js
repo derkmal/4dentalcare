b05a25dc3cd1713795f61f341cdecb821071afe8
darkmal <vagnerwillians@hotmail.com>
2023-02-26 13:21:52 -0300

    26-02-2023 13:21hs

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -12,19 +12,19 @@ const SignIn=()=> {
 	  <form>
 		   <div className="formContainer">
 				<div>
-					<img src="images/name-vector.png" alt="name-photo" /> 
+					<img src="./images/name-vector.png" alt="name-photo" /> 
 					<input type="text" placeholder='Ihren Name schreiben'  value="" name="myname" />
 				</div>
 				<div>
-					<img src="../images/telephone-vector.png" alt="telephone-photo" />
+					<img src="./images/telephone-vector.png" alt="telephone-photo" />
 					<input type="text" placeholder='Ihre Mobilnummer schreiben' value=""  name="mobile" />
 				</div>
 				<div>
-					<img src="../images/email-vector2.png" alt="email-photo" />
+					<img src="./images/email-vector2.png" alt="email-photo" />
 					<input type="text" placeholder='Ihre E-mail schreiben' value=""  name="email" />
 				</div>
 				<div>
-					<img src="../images/password-vector2.png" alt="password-photo" />
+					<img src="./images/password-vector2.png" alt="password-photo" />
 					<input type="text" placeholder='Ihres Passwort schreiben'  value=""  name="password" />
 				</div>
 				
