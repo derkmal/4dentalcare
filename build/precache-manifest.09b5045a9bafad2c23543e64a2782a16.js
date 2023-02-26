2a52eeed782dc7820dacd4d5782f240f05381104
darkmal <vagnerwillians@hotmail.com>
2023-02-26 14:22:04 -0300

    json

---
+++ build/precache-manifest.09b5045a9bafad2c23543e64a2782a16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d42f8ffb86502a1970cb",
+    "url": "/4dentalcare/static/css/main.59740027.chunk.css"
+  },
+  {
+    "revision": "d42f8ffb86502a1970cb",
+    "url": "/4dentalcare/static/js/main.d42f8ffb.chunk.js"
+  },
+  {
+    "revision": "61e6a26294e1cdead0f7",
+    "url": "/4dentalcare/static/js/1.61e6a262.chunk.js"
+  },
+  {
+    "revision": "559880fca151a3b8ca69",
+    "url": "/4dentalcare/static/js/runtime~main.559880fc.js"
+  },
+  {
+    "revision": "0ad88de163031ed1d0b04236c9f05c53",
+    "url": "/4dentalcare/static/media/dental-treatment.0ad88de1.jpg"
+  },
+  {
+    "revision": "b06513f3754c68c12be6f92d666edaba",
+    "url": "/4dentalcare/index.html"
+  }
+];
